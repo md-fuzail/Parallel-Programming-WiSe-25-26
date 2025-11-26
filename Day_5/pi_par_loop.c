2947d20a52b6437a7ea70c1b7b2a158f61da0695
Md Fuzail <fuzail7201@gmail.com>
2025-11-26 12:20:18 +0100

    Changes to pi_par_loop

--- Day_5/pi_par_loop.c
+++ Day_5/pi_par_loop.c
@@ -9,13 +9,28 @@ int main() {
     double x, pi, value, sum = 0.0;
     step = 1.0 / (double) num_steps;
 
+    omp_sched_t kind;
+    int chunk;
+    omp_get_schedule(&kind, &chunk);
+
+    const char *schedule_name;
+    switch (kind) {
+        case omp_sched_static:   schedule_name = "static"; break;
+        case omp_sched_dynamic:  schedule_name = "dynamic"; break;
+        case omp_sched_guided:   schedule_name = "guided"; break;
+        case omp_sched_auto:     schedule_name = "auto"; break;
+        default:                  schedule_name = "unknown"; break;
+    }
+
+    printf("Schedule being used: %s, Chunk size: %d\n", schedule_name, chunk);
+
     double start = omp_get_wtime();
 
-    #pragma omp parallel for schedule(runtime) reduction(+:sum)
+    #pragma omp parallel for schedule(runtime)
     for (i = 0; i < num_steps; i++) {
         x = (i + 0.5) * step;
         double value = 4.0 / (1.0 + x * x);
-        
+
         #pragma omp atomic
         sum += value;
     }
@@ -23,7 +38,7 @@ int main() {
     pi = step * sum;
     double end = omp_get_wtime();
 
-    printf("Pi (static): %.10f\n", pi);
+    printf("Pi: %.10f\n", pi);
     printf("Elapsed time: %f seconds\n", end - start);
 
     return 0;
