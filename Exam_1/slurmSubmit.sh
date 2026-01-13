e2cfb9bb17fff1148c53ce7a07b283a052fc64cf
Md Fuzail <fuzail7201@gmail.com>
2026-01-13 10:35:40 +0100

    slurm added

---
+++ Exam_1/slurmSubmit.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+####### Mail Notify / Job Name / Comment #######
+#SBATCH --job-name="fib_tasks"
+
+####### Partition #######
+#SBATCH --partition=all
+
+####### Ressources #######
+#SBATCH --time=0-00:05:00
+
+####### Node Info #######
+#SBATCH --exclusive
+#SBATCH --nodes=1
+
+####### Output #######
+#SBATCH --output=/home/fd0002114/out/heatmap_analysis.out.%j
+#SBATCH --error=/home/fd0002114/out/heatmap_analysis.err.%j
+
+#cd /path/to/bin
+./heatmap_analysis 1024 786 123 0 100 55 0 1 100
