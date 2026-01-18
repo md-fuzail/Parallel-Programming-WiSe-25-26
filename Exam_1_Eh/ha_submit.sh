2eea4c1378cd97fd96ea0715d7d7abad8b3bd9cc
Md Fuzail <fuzail7201@gmail.com>
2026-01-18 19:32:25 +0100

    slurm

--- Exam_1_Eh/ha_submit.sh
+++ Exam_1_Eh/ha_submit.sh
@@ -13,8 +13,8 @@
 #SBATCH --nodes=1
 
 ####### Output #######
-#SBATCH --output=/home/fd0001542/out/heatmap_analysis.out.%j
-#SBATCH --error=/home/fd0001542/out/heatmap_analysis.err.%j
+#SBATCH --output=/home/fd0002114/out/heatmap_analysis.out.%j
+#SBATCH --error=/home/fd0002114/out/heatmap_analysis.err.%j
 
 #cd /path/to/bin
 srun ./heatmap_analysis 1024 786 123 0 100 55 0 1 100
