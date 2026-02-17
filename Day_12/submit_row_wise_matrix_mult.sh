6c59477e7fec1a061b2a4204b5bacbf0fcb809a2
Md Fuzail <fuzail7201@gmail.com>
2026-02-17 17:57:10 +0100

    exam2

--- Day_12/submit_row_wise_matrix_mult.sh
+++ Day_12/submit_row_wise_matrix_mult.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ####### Mail Notify / Job Name / Comment #######
-#SBATCH --job-name="matmul"
+#SBATCH --job-name="row_wise_matrix_mult"
 
 ####### Partition #######
 #SBATCH --partition=all
@@ -14,8 +14,8 @@
 #SBATCH --ntasks-per-node=64
 
 ####### Output #######
-#SBATCH --output=/home/fd0002114/out/matmul.out.%j
-#SBATCH --error=/home/fd0002114/out/matmul.err.%j
+#SBATCH --output=/home/fd0002114/out/row_wise_matrix_mult.out.%j
+#SBATCH --error=/home/fd0002114/out/row_wise_matrix_mult.err.%j
 
 #cd /path/to/bin
 mpirun ./row_wise_matrix_mult
